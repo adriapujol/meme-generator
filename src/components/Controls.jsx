4e908009cdffb3d61015aa724b0e3fd5efe15730
Adria Pujol <adriapujolaparicio@gmail.com>
2022-03-30 21:32:46 +0200

    input text underline animation

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,13 +3,16 @@ import './Controls.scss';
 
 function controls() {
     return (
-        <div>
+        <div className='controls'>
             <input type='text' id='img-url' name='img-url' placeholder='enter image url' />
+            <span className='underline'></span>
             <button>upload</button>
 
             <input type='text' id='top-text' name='top-text' placeholder='funny top text here' />
+            <span className='underline'></span>
             <button>clear</button>
             <input type='text' id='bottom-text' name='bottom-text' placeholder='funny bottom text here' />
+            <span className='underline'></span>
             <button>clear</button>
 
             <label htmlFor='font'>font:</label>
@@ -21,6 +24,8 @@ function controls() {
             <label htmlFor="font-size">font size:</label>
             <button>-</button>
             <button>+</button>
+
+            <button>download</button>
         </div>
     )
 }
