5c577519f1d3d03a8586f32ae402cc208dbf27b9
Adria Pujol <adriapujolaparicio@gmail.com>
2022-03-30 22:49:42 +0200

    button animation

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -10,32 +10,31 @@ function controls() {
                     <input type='text' id='img-url' name='img-url' placeholder='enter image url' />
                     <span className='underline'></span>
                 </div>
-                <button>upload</button>
+                <button className='btn-txt'>upload</button>
             </div>
             <div className="block">
-
                 <div className="input-box">
                     <input type='text' id='top-text' name='top-text' placeholder='funny top text here' />
                     <span className='underline'></span>
                 </div>
-                <button>clear</button>
+                <button className='btn-txt'>X</button>
             </div>
             <div className="block">
                 <div className="input-box">
                     <input type='text' id='bottom-text' name='bottom-text' placeholder='funny bottom text here' />
                     <span className='underline'></span>
                 </div>
-                <button>clear</button>
+                <button className='btn-txt'>X</button>
             </div>
             <div className="block">
                 <label htmlFor='font'>font:</label>
                 <div>Select font</div>
             </div>
-            <div className="bloc">
+            <div className="block">
                 <label htmlFor='color'>color:</label>
                 <input type='color' value='#ffffff' />
             </div>
-            <div className="bloc">
+            <div className="block">
                 <label htmlFor="font-size">font size:</label>
                 <button>-</button>
                 <button>+</button>
