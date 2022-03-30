bb6dbd16d510b1b4b61d0f7f0071c92a2e3cb52a
Adria Pujol <adriapujolaparicio@gmail.com>
2022-03-30 22:03:22 +0200

    control div structure plus bangers font

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -4,26 +4,43 @@ import './Controls.scss';
 function controls() {
     return (
         <div className='controls'>
-            <input type='text' id='img-url' name='img-url' placeholder='enter image url' />
-            <span className='underline'></span>
-            <button>upload</button>
 
-            <input type='text' id='top-text' name='top-text' placeholder='funny top text here' />
-            <span className='underline'></span>
-            <button>clear</button>
-            <input type='text' id='bottom-text' name='bottom-text' placeholder='funny bottom text here' />
-            <span className='underline'></span>
-            <button>clear</button>
+            <div className="block">
+                <div className="input-box">
+                    <input type='text' id='img-url' name='img-url' placeholder='enter image url' />
+                    <span className='underline'></span>
+                </div>
+                <button>upload</button>
+            </div>
+            <div className="block">
 
-            <label htmlFor='font'>font:</label>
-            <div>Select font</div>
+                <div className="input-box">
+                    <input type='text' id='top-text' name='top-text' placeholder='funny top text here' />
+                    <span className='underline'></span>
+                </div>
+                <button>clear</button>
+            </div>
+            <div className="block">
+                <div className="input-box">
+                    <input type='text' id='bottom-text' name='bottom-text' placeholder='funny bottom text here' />
+                    <span className='underline'></span>
+                </div>
+                <button>clear</button>
+            </div>
+            <div className="block">
+                <label htmlFor='font'>font:</label>
+                <div>Select font</div>
+            </div>
+            <div className="bloc">
+                <label htmlFor='color'>color:</label>
+                <input type='color' value='#ffffff' />
+            </div>
+            <div className="bloc">
+                <label htmlFor="font-size">font size:</label>
+                <button>-</button>
+                <button>+</button>
+            </div>
 
-            <label htmlFor='color'>color:</label>
-            <input type='color' value='#ffffff' />
-
-            <label htmlFor="font-size">font size:</label>
-            <button>-</button>
-            <button>+</button>
 
             <button>download</button>
         </div>
