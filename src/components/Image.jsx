c9a5dd72ef5c9ce3eb1f6b293a49a9a2559a1bfb
Adria Pujol <adriapujolaparicio@gmail.com>
2022-03-31 20:17:12 +0200

    color text working, text meme positioned, font family working

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,12 +5,18 @@ import picture2 from '../img/interesting_men.jpg';
 
 function Image({ imgUrl, topText, bottomText, fontColor, fontSize, fontType }) {
 
+    const style = {
+        color: fontColor,
+        fontSize: `${fontSize}rem`
+    }
+
+
     return (
         <figure>
-            <p className={`meme-top-text .${fontType}`}>{topText}</p>
+            <p style={style} className={`meme-top-text ${fontType}`}>{topText}</p>
             <img src={picture} alt="to be memed" />
-            <p className={`meme-bottom-text .${fontType}`}>{bottomText}</p>
-        </figure>
+            <p style={style} className={`meme-bottom-text ${fontType}`}>{bottomText}</p>
+        </figure >
     )
 }
 
