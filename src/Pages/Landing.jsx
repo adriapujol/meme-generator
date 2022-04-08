d5985d53f27e0055e23ed7519258652597cb0924
Adria Pujol <adriapujolaparicio@gmail.com>
2022-04-08 05:38:16 +0200

    title responsive

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -9,10 +9,12 @@ function Landing({ makeMeme, setMakeMeme }) {
 
     return (
         <section className='landing'>
-            <div className={makeMeme ? "move title-box" : "title-box"} >
-                <h1 className={makeMeme ? "smaller" : ""}>ME<span>ME</span></h1>
+            <h1 className={makeMeme ? "smaller" : ""}>ME<span>ME</span></h1>
+            <div className="subtitle">
                 <h3 className={makeMeme ? "bye-smaller" : ""}>generator</h3>
             </div>
+            {/* <div className={makeMeme ? "move title-box" : "title-box"} >
+            </div> */}
             <button className={makeMeme ? "bye main-btn" : "main-btn"} onClick={handleClick}>MAKE A MEME</button>
         </section>
     )
