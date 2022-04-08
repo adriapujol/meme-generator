4d5efba1a05272c61ce84bc0f7c5860113ead7b0
Adria Pujol <adriapujolaparicio@gmail.com>
2022-04-08 07:32:11 +0200

    element clean up and not used animations erased

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -10,12 +10,10 @@ function Landing({ makeMeme, setMakeMeme }) {
     return (
         <section className='landing'>
             <h1 className={makeMeme ? "smaller" : ""}>ME<span>ME</span></h1>
-            <div className="subtitle">
-                <h3 className={makeMeme ? "bye-smaller" : ""}>generator</h3>
-            </div>
-            {/* <div className={makeMeme ? "move title-box" : "title-box"} >
-            </div> */}
-            <button className={makeMeme ? "bye main-btn" : "main-btn"} onClick={handleClick}>MAKE A MEME</button>
+            {!makeMeme && <div className="subtitle">
+                <h3>generator</h3>
+            </div>}
+            {!makeMeme && <button className="main-btn" onClick={handleClick}>MAKE A MEME</button>}
         </section>
     )
 }
